bda3bc8e4afd0eaafa027d1ad7432360deb8a3b7
attekeit <39433971+attekeit@users.noreply.github.com>
2018-06-16 12:10:53 +0300

    Lisättiin herokun tarjoama portti

--- src/main/java/drinkit/drinkit/Main.java
+++ src/main/java/drinkit/drinkit/Main.java
@@ -27,6 +27,12 @@ import spark.template.thymeleaf.ThymeleafTemplateEngine;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        
+        // asetetaan portti jos heroku antaa PORT-ympäristömuuttujan
+        if (System.getenv("PORT") != null) {
+            Spark.port(Integer.valueOf(System.getenv("PORT")));
+        }
+
 
         Database database = new Database("jdbc:sqlite:juomat.db");
         Connection connection = database.getConnection();
